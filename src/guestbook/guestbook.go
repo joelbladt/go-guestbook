1959cbaea63f4b7bd1530ef1ee56e9097d766ae1
joelbladt <joel@bladt.de>
2025-04-13 20:00:19 +0200

    Write Save() method in guestbook package

--- src/guestbook/guestbook.go
+++ src/guestbook/guestbook.go
@@ -1,8 +1,35 @@
 package guestbook
 
+import (
+	"os"
+	"strings"
+	"time"
+)
+
 const guestbookFile = "guestbook.txt"
 
-func Save(name string, message string) {
+func Save(name string, message string) error {
+	// trim spaces
+	name = strings.TrimSpace(name)
+	message = strings.TrimSpace(message)
+
+	// if name or message empty then ignore
+	if name == "" || message == "" {
+		return nil
+	}
+
+	// concatenate string
+	var newEntry string = "[" + time.Now().Format(time.RFC822) + "] " + name + ": " + message + "\n"
+
+	// open file
+	f, err := os.OpenFile(guestbookFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(newEntry)
+	return err
 }
 
 func Show() {
