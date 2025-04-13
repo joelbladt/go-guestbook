49828e93e39138a1faccfc700167071b2e4c8911
joelbladt <joel@bladt.de>
2025-04-13 20:44:15 +0200

    add post method to save entries

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"guestbook/src/guestbook"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,7 +11,17 @@ var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+		if r.Method == http.MethodPost {
+			var name string = r.FormValue("name")
+			var message string = r.FormValue("message")
+			err := guestbook.Save(name, message)
+			if err != nil {
+				http.Error(w, "Error during saving", http.StatusInternalServerError)
+				return
+			}
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
 	})
 
 	log.Println("Server running at http://localhost:8080")
