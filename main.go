58eda3111241a4d2a8c0d0b9690e420308433f20
joelbladt <joel@bladt.de>
2025-04-13 20:39:16 +0200

    init net/http service

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
 func main() {
-	// Start here...
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	log.Println("Server running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
